c2b3f4747f845fe66ad7fb52d23e6ceb75bb0dc9
Hiroshi Ouchiyama <hiouchiy@outlook.jp>
2021-08-27 15:36:23 +0900

    Update

--- azure_functions/powershell/register-SP.ps1
+++
@@ -1,5 +0,0 @@
-﻿Update-AzFunctionApp -Name ovaas-ps-experiment -ResourceGroupName OVaaS_Experiments -IdentityType SystemAssigned
-
-$SP=(Get-AzADServicePrincipal -DisplayName ovaas-ps-experiment).Id
-$RG=(Get-AzResourceGroup -Name OVaaS_Experiments).ResourceId
-New-AzRoleAssignment -ObjectId $SP -RoleDefinitionName "Contributor" -Scope $RG
